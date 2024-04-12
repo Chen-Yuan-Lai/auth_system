958598394f9bc9e4bc00cea1af5db86703d55d03
Cheng-Yuan-Lai <a186235@g,ail.com>
2024-04-12 22:08:22 +0800

    feat: create an customer exception class

---
+++ src/exceptions.py
@@ -0,0 +1,18 @@
+from typing import Any, Dict, Union
+
+from fastapi import HTTPException
+
+
+class NewHTTPException(HTTPException):
+    def __init__(
+        self,
+        status_code: int,
+        detail: Any = None,
+        headers: Union[Dict[str, Any], None] = None,
+        msg: str = None,
+    ) -> None:
+        super().__init__(status_code, detail, headers)
+        if msg:
+            self.msg = msg
+        else:
+            self.msg = detail
