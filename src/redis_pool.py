b129af6dff6e66ee333b458d66b5513b494c3f26
Cheng-Yuan-Lai <a186235@g,ail.com>
2024-04-12 22:08:53 +0800

    feat: create a redis connection pool

---
+++ src/redis_pool.py
@@ -0,0 +1,7 @@
+import redis.asyncio as redis
+from .config import settings
+
+REDIS_URL = str(settings.REDIS_URL)
+
+pool = redis.ConnectionPool.from_url(REDIS_URL)
+redis_client = redis.Redis(connection_pool=pool)
