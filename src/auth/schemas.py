1e6685ac30fce942d395afd722b4e55781988a7e
Cheng-Yuan-Lai <a186235@g,ail.com>
2024-04-14 22:26:37 +0800

    feat: rewrite schema validator to produce custom error messages and add sample request into swagger docs

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -4,15 +4,25 @@ from pydantic import BaseModel, Field, field_validator
 
 
 class User(BaseModel):
-    username: str = Field(min_length=3, max_length=32)
-    password: str = Field(
-        min_length=8,
-        max_length=32,
-    )
+    username: str = Field(..., max_length=32, min_length=3)
+    password: str = Field(..., max_length=32, min_length=3)
 
     @field_validator("password")
-    def regexMatch(cls, p: str) -> str:
+    def password_checker(cls, p: str) -> str:
         reForPassword: re.Pattern[str] = re.compile(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)")
         if not reForPassword.match(p):
-            raise ValueError("Invalid password")
+            raise ValueError(
+                "Invalid password: must include at least one lowercase letter, one uppercase letter, and one digit."
+            )
         return p
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [{"username": "Ian", "password": "Aa1234567"}]
+        }
+    }
+
+
+class Item(BaseModel):
+    success: bool
+    reason: str
